0cf27bfac0b83ee9b41c100bcc2074b6adaf72c1
Nina Jade <jaderiver64@gmail.com>
2021-03-04 13:29:00 -0600

    adding Vote model

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,13 +1,9 @@
 const router = require('express').Router();
-const { User, Post } = require('../../models');
-const userRoutes = require('./user-routes.js');
-// create associations
-User.hasMany(Post, {
-    foreignKey: 'user_id'
-});
-Post.belongsTo(User, {
-    foreignKey: 'user_id',
-});
+
+const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes');
+
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
