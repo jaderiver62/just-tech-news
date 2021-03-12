69000fd726d217137d44afde33cf0b1007e307a9
Nina Jade <jaderiver64@gmail.com>
2021-03-12 16:31:28 -0600

    adding url_format helper and tests

--- utils/helpers.js
+++ utils/helpers.js
@@ -10,5 +10,14 @@ module.exports = {
         }
 
         return word;
-    }
+    },
+    format_url: url => {
+        return url
+            .replace('http://', '')
+            .replace('https://', '')
+            .replace('www.', '')
+            .split('/')[0]
+            .split('?')[0];
+    },
+
 }
